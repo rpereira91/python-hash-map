af85281ab87432871d0440b234127c9a7089ff3d
rpereira91 <boveah@gmail.com>
2019-06-21 14:10:12 -0400

    added comments

--- HashMap.py
+++ HashMap.py
@@ -1,19 +1,29 @@
 class HashMap(object):
     """docstring for HashMap."""
     def __init__(self):
+        # create an array of buckets, normally buckets will be n^2
         self.hashmap = [[] for i in range(256)]
-    
+    #function to insert objects 
     def insert_item(self, item):
+        #create the hash key using the built in hash function and moduloing it by the length of the hashmap
+        # this ensures that it is within the range of the array size 
         hash_key = hash(item.get_key()) % len(self.hashmap)
+        #boolean to make sure that key is unique
         key_exists = False
+        #get the bucket that is in the index of the hash key
         bucket = self.hashmap[hash_key]
+        #enumerate through the bucket looking for the key
         for index, obj in enumerate(bucket):
+            #get they key of the object
             key = obj.get_key()
+            #if the key exists we know to update the object
             if item.get_key() == key:
                 key_exists = True
                 break
+        #update the object
         if key_exists:
-            bucket[index] = (item)
+            bucket[index].set_value(item.get_value())
+        #add the object to the bucket list
         else:
             bucket.append(item)
 
@@ -21,7 +31,7 @@ class HashMap(object):
         hash_key = hash(key) % len(self.hashmap)
         bucket = self.hashmap[hash_key]
         for i, obj in enumerate(bucket):
-            return obj.get_value()
+            return obj
 
 class Item(object):
     """docstring for Item."""
@@ -33,10 +43,15 @@ class Item(object):
         return self.value
     def get_key(self):
         return self.key
-    
-hm = HashMap()
-hm.insert_item(Item("001", "Item test"))
-hm.insert_item(Item("010", "01 test"))
+    def set_value(self, value):
+        self.value = value
+    def print_object(self):
+        print("Key : {} \tValue: {}".format(self.key, self.value))
 
-print(hm.get_item_value("001"))
-print(hm.get_item_value("010"))
+if __name__ == "__main__":
+    hm = HashMap()
+    hm.insert_item(Item("001", "Some random value"))
+    hm.insert_item(Item("010", "Another value or whatever"))
+
+    hm.get_item_value("001").print_object()
+    hm.get_item_value("010").print_object()
